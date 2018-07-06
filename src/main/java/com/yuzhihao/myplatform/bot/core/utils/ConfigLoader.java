770301b8bb19ef037f4fc0ea75d4471a471f2847
余知昊 <928435030@qq.com>
2018-07-06 10:00:48 +0800

    Update ConfigLoader.java

--- src/main/java/com/yuzhihao/myplatform/bot/core/utils/ConfigLoader.java
+++ src/main/java/com/yuzhihao/myplatform/bot/core/utils/ConfigLoader.java
@@ -5,9 +5,37 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 public class ConfigLoader {
 
 
-    public static JSONPObject loadConfigFromJson(){
-        return null;
+   /**
+     * 指定路径加载本地json文件
+     * @param path
+     * @return
+     */
+    public static String loadJsonFromFile(String path){
+        StringBuffer sb = new StringBuffer();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String s = null;
+            while((s =reader.readLine())!=null){
+                sb.append(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
+
+    public static Executor getExecutorByName(String classname){
+        Executor executor = null;
+        try {
+            Class clazz = Class.forName(classname);
+            Constructor constructor = clazz.getConstructor();
+            executor = (Executor) clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return executor;
+    }
+
 
     //public static MiddlewareManager initMiddlewareManager(){return null;}
 }
